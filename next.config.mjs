eb7e308edf8ea5fcfc80910dc87e0eb31e774963
nihaltowfiq <nihaltowfiq@gmail.com>
2024-05-28 22:03:27 +0600

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/my-booking',
   webpack: function (config) {
     config.module.rules.push({
       test: /\.svg$/i,
